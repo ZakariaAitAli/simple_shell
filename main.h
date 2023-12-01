b1de5180a3bab5f3496bab24e78e45927f0d1c65
Zakaria AIT ALI <zakaria.aitali@etu.uae.ac.ma>
2023-12-01 16:23:27 +0100

    Created main header file

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef SIMPLE_SHELL_MAIN_H
+#define SIMPLE_SHELL_MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+
+
+#endif /* SIMPLE_SHELL_MAIN_H */
