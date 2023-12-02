126716c7f5af3a757f09021d66fefe18c0a61690
Zakaria AIT ALI <zakaria.aitali@etu.uae.ac.ma>
2023-12-02 21:40:27 +0100

    Update main header file

--- main.h
+++ main.h
@@ -5,8 +5,18 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
 
+#define MAX_TOKEN 512
+#define BUFFER_SIZE 1024
 
+void handle_memory_allocation_error(void);
+void handle_fork_error(void);
+void handle_execve_error(void);
+
+char **tokenize_input(char *input_buffer, const char *delimiters);
+char *get_executable_path(const char *command);
 
 #endif /* SIMPLE_SHELL_MAIN_H */
