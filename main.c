5b1959cb97c129e8e9181a24858c213a8d7cd4ff
Zakaria AIT ALI <zakaria.aitali@etu.uae.ac.ma>
2023-12-08 20:05:38 +0100

    Restart

--- main.c
+++ main.c
@@ -1,19 +1,2 @@
-#include "shell.h"
 
-int main(void)
-{
-	char *line;
-	char *args[MAX_TOKENS];
-	int status;
 
-	do {
-		write(1, "($) ", 4);
-		line = _getline();
-		_split_line(line, args);
-		status = _execute(args);
-
-		_free_memory(line);
-	} while (status);
-
-	return 0;
-}
--- shell.h
+++ shell.h
@@ -1,22 +1,5 @@
 #ifndef SHELL_H
 #define SHELL_H
 
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <dirent.h>
-
-#define MAX_TOKENS 64
-#define BUFFER_SIZE 1024
-
-/* Function prototypes */
-char *_getline(void);
-void _split_line(char *line, char **tokens);
-int _execute(char **args);
-void _free_memory(char *ptr);
-void _perror(const char *msg);
 
 #endif /* SHELL_H */
